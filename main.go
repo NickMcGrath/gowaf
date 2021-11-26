3eee79a3d5bddb3d67d4fb13a9824855a1045bc8
NickMcGrath <nickmcgrath@live.ca>
2021-11-25 22:06:22 -0800

    yeet

--- main.go
+++ main.go
@@ -1,84 +1,94 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"syscall/js"
 	"time"
 )
 
-//NodePack must be added to all components
-type NodePack struct {
-	getNode func() *js.Value
-	setNode func(innerHTML string)
+type nodeWrapper struct {
+	node       *js.Value
+	nodeString string
+}
+
+func (n *nodeWrapper) getNode() *js.Value {
+	return n.node
+}
+func (n *nodeWrapper) setNode(node *js.Value) {
+	n.node = node
+}
+func (n *nodeWrapper) newNode(nodeType string, parentId string) {
+	dom := js.Global().Get("document")
+	newElement := dom.Call("createElement", nodeType)
+	body := dom.Call("getElementById", parentId)
+	body.Call("appendChild", newElement)
+	n.node = &newElement
+}
+func (n *nodeWrapper) setInnerHTML(innerHTML ...string) {
+	n.nodeString = strings.Join(innerHTML, "")
+	n.node.Set("innerHTML", strings.Join(innerHTML, ""))
+}
+func (n *nodeWrapper) String() string {
+	return n.nodeString
 }
 
 //NodePackFactory gives NodePack the functions needed
 type NodePackFactory struct {
-	node *js.Value
 }
 
-func (nf *NodePackFactory) newNode(nodeType string) {
-	dom := js.Global().Get("document")
-	newElement := dom.Call("createElement", nodeType)
-	body := dom.Call("getElementById", "bodyId")
-	body.Call("appendChild", newElement)
-	nf.node = &newElement
-}
-func (nf *NodePackFactory) getNode() *js.Value {
-	return nf.node
-}
-
-func (nf *NodePackFactory) setNode(innerHTML string) {
-	nf.node.Set("innerHTML", innerHTML)
+func (nf *NodePackFactory) newNode(nodeType string, parentId string) *nodeWrapper {
+	var nw nodeWrapper
+	nw.newNode(nodeType, parentId)
+	return &nw
 }
 
 //MyComponent is an example of a users class.
 //They can do what ever they want and when ever they want to render they call nodePack.SetNode()
 type myComponent struct {
-	nodePack         NodePack
+	nodeWrap         *nodeWrapper
 	title            string
 	data             string
 	myOtherComponent myOtherComponent
 }
 
-func (myc myComponent) Render() {
-	myc.nodePack.setNode("<h1>" + myc.title + "</h2>" +
-		"<p>" + myc.data + "</p>" + myc.myOtherComponent.nodePack.getNode().String())
+func (myc *myComponent) Render() {
+	fmt.Println("<h1>", myc.title, "</h2>",
+		"<p>", myc.data, "</p>", myc.myOtherComponent.nodeWrap.String())
+
+	myc.nodeWrap.setInnerHTML("<h1>", myc.title, "</h2>",
+		"<p>", myc.data, "</p>", myc.myOtherComponent.nodeWrap.String())
 }
 
 type myOtherComponent struct {
-	nodePack  NodePack
+	nodeWrap  *nodeWrapper
 	paragraph string
 }
 
-func (myc myOtherComponent) Render() {
-	myc.nodePack.setNode("<h1>" + myc.paragraph + "</h2>")
+func (myc *myOtherComponent) Render() {
+	myc.nodeWrap.setInnerHTML("<h1>", myc.paragraph, "</h2>")
 }
 
 func testMyComponent() {
-	nf := NodePackFactory{}
-	nf.newNode("div")
-	pack := NodePack{getNode: nf.getNode, setNode: nf.setNode}
-	nf2 := NodePackFactory{}
-	nf2.newNode("div")
-	pack2 := NodePack{getNode: nf2.getNode, setNode: nf2.setNode}
-	component2 := myOtherComponent{nodePack: pack2, paragraph: "my paragraph"}
+	var nf NodePackFactory
+	component2 := myOtherComponent{nodeWrap: nf.newNode("div", "bodyId"), paragraph: "my paragraph"}
 	component2.Render()
 	component2.paragraph = "yolo"
 
-	component := myComponent{nodePack: pack,
+	component := myComponent{
 		title:            "my component title",
 		data:             "my data",
+		nodeWrap:         nf.newNode("div", "bodyId"),
 		myOtherComponent: component2}
+
 	component.Render()
 	component.title = "new title"
 	component.data = "new data"
-	component2.Render()
 	time.Sleep(2 * time.Second)
+	component2.Render()
 	component.Render()
 }
 
 func main() {
-
 	testMyComponent()
-
 }
