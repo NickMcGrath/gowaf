5f3a595c9ca4f1550ba2a447c1d700c695a1469c
NickMcGrath <nickmcgrath@live.ca>
2021-11-25 19:32:35 -0800

    big commit baby

--- main.go
+++ main.go
@@ -34,26 +34,45 @@ func (nf *NodePackFactory) setNode(innerHTML string) {
 //MyComponent is an example of a users class.
 //They can do what ever they want and when ever they want to render they call nodePack.SetNode()
 type myComponent struct {
-	nodePack NodePack
-	title    string
-	data     string
+	nodePack         NodePack
+	title            string
+	data             string
+	myOtherComponent myOtherComponent
 }
 
 func (myc myComponent) Render() {
 	myc.nodePack.setNode("<h1>" + myc.title + "</h2>" +
-		"<p>" + myc.data + "</p>")
+		"<p>" + myc.data + "</p>" + myc.myOtherComponent.nodePack.getNode().String())
+}
+
+type myOtherComponent struct {
+	nodePack  NodePack
+	paragraph string
+}
+
+func (myc myOtherComponent) Render() {
+	myc.nodePack.setNode("<h1>" + myc.paragraph + "</h2>")
 }
 
 func testMyComponent() {
 	nf := NodePackFactory{}
 	nf.newNode("div")
 	pack := NodePack{getNode: nf.getNode, setNode: nf.setNode}
+	nf2 := NodePackFactory{}
+	nf2.newNode("div")
+	pack2 := NodePack{getNode: nf2.getNode, setNode: nf2.setNode}
+	component2 := myOtherComponent{nodePack: pack2, paragraph: "my paragraph"}
+	component2.Render()
+	component2.paragraph = "yolo"
+
 	component := myComponent{nodePack: pack,
-		title: "my component title",
-		data:  "my data"}
+		title:            "my component title",
+		data:             "my data",
+		myOtherComponent: component2}
 	component.Render()
 	component.title = "new title"
 	component.data = "new data"
+	component2.Render()
 	time.Sleep(2 * time.Second)
 	component.Render()
 }
